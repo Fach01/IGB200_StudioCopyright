f84ac67ffe78dac6030d8a3d76abf97ea47028de
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-08-13 13:54:10 +1000

    Card scriptable object set a bit up

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Card : ScriptableObject
+{
+    public string name;
+    //needs values such as price, ability, etc???
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
