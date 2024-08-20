df1a3c69e382232635a1bf982a270d2e461af4a8
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-08-20 16:12:26 +1000

    added deck and beginning of level managers

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject deck;
+    private Card[] activePlannerCards;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BeginLevel()
+    {
+        //draw 4 random cards from deck
+        // turn()
+    }
+
+    void Turn()
+    {
+        //for cards in active planner cards
+        //get planners ability and enable
+        //
+        
+    }
+
+    void LoseGame()
+    {
+        // if deck.size = 0
+        // or budget <= 0 
+        // Game over
+    }
+}
