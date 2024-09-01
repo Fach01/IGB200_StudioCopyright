1190a7f365e7ae09738b41add6c38417d335d52d
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-09-01 18:41:43 +1000

    redid some stuff

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -10,17 +10,4 @@ public abstract class Ability : ScriptableObject
     public abstract void ActivateAbility();
 }
 
-[CreateAssetMenu(menuName = "Abilities/Surveyor")]
-public class Surveyor : Ability
-{
-    private void OnEnable()
-    {
-        cost = 0;
-        description = "For every Framework card you play, double the output.";
-    }
-    public override void ActivateAbility()
-    {
-        throw new System.NotImplementedException();
 
-    }
-}
