86204c24f6a8605f47bc7ff54dcd06138111f2e7
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-09-01 10:45:12 +1000

    started ability stuff

---
+++ Assets/Scripts/Abilities/Surveyor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surveyor : Ability
+{
+    bool isActive = false;
+    public override void ActivateAbility()
+    {
+        isActive = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cost = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ability : MonoBehaviour
+public abstract class Ability : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int cost;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void ActivateAbility();
 }
