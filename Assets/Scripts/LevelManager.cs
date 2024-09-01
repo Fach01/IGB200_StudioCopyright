e180985fb88a7405f1102d9185a38860de81d8de
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-09-01 18:21:59 +1000

    arbitrary merge because liv sucks

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -114,8 +114,6 @@ public class LevelManager : MonoBehaviour
                 }
 
                 selectedCard = hit.collider.gameObject;
-                Debug.Log("Card selected: " + selectedCard.GetComponent<CardManager>().card.name);
-
             }
         }
         if (selectedCard != null)
