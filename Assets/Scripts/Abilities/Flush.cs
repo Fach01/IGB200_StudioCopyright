ad8eff313e848b7520d40e66fabe21d802f64c2f
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-09-17 23:16:57 +1000

    FLUSH WORKS

--- Assets/Scripts/Abilities/Flush.cs
+++ Assets/Scripts/Abilities/Flush.cs
@@ -8,30 +8,32 @@ public class Flush : BuilderAbility
     public override void ActivateAbility(PlayerManager playerManager)
     {
         playerManager.playField.GetComponent<Button>().interactable = false;
-        GameObject cardChosen = null;
         int numDraw = 2;
-        StartCoroutine(SelectCard(null, playerManager, numDraw));
+        StartCoroutine(SelectCard(playerManager, numDraw));
     }
 
 
     //TODO check whether the card being played is still activated and if thats the issue
-    private IEnumerator SelectCard(GameObject cardChosen, PlayerManager playerManager, int numDraw)
+    IEnumerator SelectCard(PlayerManager playerManager, int numDraw)
     {
-        Debug.Log("Flush is active");
-        if (cardChosen == null)
+        while (playerManager.selectedCard == null)
         {
-            if (playerManager.selectedCard != null ) // && card is in hand
-            {
-                cardChosen = playerManager.selectedCard;
-                playerManager.DiscardCard();
-
-                for (int i = 0; i < numDraw; i++)
-                {
-                    playerManager.deck.GetComponent<DeckManager>().DrawCard();
-                }
-                playerManager.playField.GetComponent<Button>().interactable = true;
-            }
+            Debug.Log("Waiting...");
+            yield return null;
         }
-        yield return null;
+
+        if (playerManager.selectedCard != null)
+        {
+            Destroy(playerManager.selectedCard);
+
+
+            for (int i = 0; i < numDraw; i++)
+            {
+                playerManager.deck.GetComponent<DeckManager>().DrawCard();
+            }
+            playerManager.playField.GetComponent<Button>().interactable = true;
+
+            yield break;
+        }
     }
 }
