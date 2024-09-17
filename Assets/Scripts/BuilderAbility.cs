b2a100f8b28a6b0a341a491b21c3275ae61bba46
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-09-17 14:44:49 +1000

    set up

---
+++ Assets/Scripts/BuilderAbility.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BuilderAbility : MonoBehaviour, IAbility
+{
+    public int Cost { get; set; }
+
+    public string Description { get; set; }
+
+    public abstract void ActivateAbility();
+}
---
+++ Assets/Scripts/IAbility.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public interface IAbility
+{
+    public int Cost { get; set; }
+    public string Description { get; set; }
+
+    public void ActivateAbility();
+}
+
+
