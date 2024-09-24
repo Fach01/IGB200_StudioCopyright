649265be7e85f9c5ee93c9ef427741e80e0707df
Fach01 <peytenredburn01@gmail.com>
2024-09-25 08:58:39 +1000

    ReOrganised Folders+ added Board + text animations

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -119,7 +119,6 @@ public class LevelManager : MonoBehaviour
         StartCoroutine(PlayPhaseAnimation());
         // TODO: add budged turn loss text animation
 
-        
 
         if (utilitiesCount >= utilitiesGoal && frameworksCount >= frameworksGoal)
         {
@@ -175,83 +174,95 @@ public class LevelManager : MonoBehaviour
 
             if (playFieldManager.cards[i] == null)
             {
-                //set the temporary pools as equal to 0 then play the the tally gains and losses animation
-               
-
-                StartCoroutine(TallyGainsAndLosses());
+                // open Set EndTurnAnimation to active then wait for entry animation
                 UIManager.EndTurnAnimation.SetActive(true);
+                yield return new WaitForSeconds(0.3f);
+
+                //then start the tallying of the gains and losses
+                StartCoroutine(TallyGainsAndLosses());
+
                 yield break;
             }
             playFieldManager.cards[i].GetComponent<CardManager>().cardanimator.SetBool("Add Resource", true);
-            yield return new WaitForSeconds(.2f);
+            yield return new WaitForSeconds(.2f); 
         }
     }
     IEnumerator TallyGainsAndLosses()
     {
-        // animation will play at the start of the play phase to show all cards in play
         for (int i = 0; i < playFieldManager.cards.Count; i++)
         {
-            yield return new WaitForSeconds(1f);
-
-
-            UIManager.EndTurnAnimation.GetComponent<Animator>().SetBool("Gives Frames",false);
+            yield return new WaitForSeconds(1f); // waits at the start to keep consistent timing
 
             if (playFieldManager.cards[i] == null)
             {
-                UIManager.EndTurnAnimation.SetActive(false);
+                //after animation deactivate EndTurnPanel
+                UIManager.EndTurnAnimation.GetComponent<Animator>().SetBool("Exit", true);
 
+                //waits for exit animation to play before resuming with the rest of the code
+                yield return new WaitForSeconds(1f);
+
+                //apply changes in budge frames and utilities
                 turnBudget -= tempBudget;
                 utilitiesCount += tempUtil;
                 frameworksCount += tempFrames;
 
-                ResetTempPools();
+                ResetTempPools(); // reset the temporary pools
 
-                UIManager.SetBudgetText(turnBudget.ToString());
+                // Displays changes to the UI
+                UIManager.SetBudgetText(turnBudget.ToString()); 
                 UIManager.SetUtilitiesText(utilitiesCount.ToString());
                 UIManager.SetFrameworksText(frameworksCount.ToString());
                 
                 yield break;
             }
-            //card animation plays
 
             int cost = playFieldManager.cards[i].GetComponent<CardManager>().m_card.cost;
             int utilities = playFieldManager.cards[i].GetComponent<CardManager>().m_card.utilities;
             int frameworks = playFieldManager.cards[i].GetComponent<CardManager>().m_card.frameworks;
-
-            // if the card gives utitlities or frameworks an animation will play
 
             tempBudget -= cost;
             tempUtil += utilities;
             tempFrames += frameworks;
 
             UIManager.DisplayTemporaryBudget(tempBudget);
-            UIManager.DisplayTemporaryFramework(tempUtil);
-            UIManager.DisplayTemporaryUtilities(tempFrames);
+            UIManager.DisplayTemporaryUtilities(tempUtil);
+            UIManager.DisplayTemporaryFramework(tempFrames);
 
+            // if the card gives utilities or frameworks an animation will play
             if (utilities > 0)
             {
-                Debug.Log(utilities);
-                UIManager.EndTurnAnimation.GetComponent<Animator>().SetBool("Gives Frames", true);
+                Debug.Log("utilities:" + utilities);
+                UIManager.EndTurnAnimation.GetComponent<Animator>().SetBool("Gives Util", true); 
+               
 
             }
 
             if (frameworks > 0)
             {
-                //Debug.Log(frameworks);
-                UIManager.EndTurnAnimation.GetComponent<Animator>().SetBool("Gives Util", true);
+                Debug.Log("Frameworks:" + frameworks);
+                UIManager.EndTurnAnimation.GetComponent<Animator>().SetBool("Gives Frames", true);
 
             }
-            yield return new WaitForSeconds(0.1f);
+
+            if (tempUtil > 5)
+            {
+                
+            }
+
+            //wait for 0.05 seconds to turn off animation to stop looping
+            yield return new WaitForSeconds(0.05f);
             UIManager.EndTurnAnimation.GetComponent<Animator>().SetBool("Gives Util", false);
             UIManager.EndTurnAnimation.GetComponent<Animator>().SetBool("Gives Frames", false);
         }
     }
     public void ResetTempPools()
     {
+        //sets all temporary pools as = 0
         tempBudget = 0;
         tempUtil = 0;
         tempFrames = 0;
 
+        //sets all UI temp pools as = 0
         UIManager.DisplayTemporaryBudget(tempBudget);
         UIManager.DisplayTemporaryFramework(tempUtil);
         UIManager.DisplayTemporaryUtilities(tempFrames);
