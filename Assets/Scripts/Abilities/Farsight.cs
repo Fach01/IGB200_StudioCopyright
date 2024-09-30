cd912181d8729b20b7164a25bb7702ec26b0fd7d
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-09-30 23:01:11 +1000

    implementation notes

--- Assets/Scripts/Abilities/Farsight.cs
+++ Assets/Scripts/Abilities/Farsight.cs
@@ -7,8 +7,19 @@ public class Farsight : BuilderAbility
     public override string Description { set { value = desc; } get { return desc; } }
     public string desc = "look at the top 4 cards in your deck and replace a card in your hand with one of them";
 
+    public GameObject deckSnapshot;
+
     public override void ActivateAbility(PlayerManager playerManager, GameObject AbilityUI)
     {
-        Debug.Log("Wooowww foundation");
+        deckSnapshot.SetActive(true);
+        for (int i = 0; i < 4; i++)
+        {
+            //draw a card
+            //set it in a slot in deck snapshot
+            //make sure no other cards are clickable
+            //start a coroutine while you wait to select a card to add to your hand, and a card from hand to replace
+            //once selection confirmed, add card objects of the 3 unchosen cards, and the card in the hand back into the deck
+            //then destroy their gameobjects
+        }
     }
 }
