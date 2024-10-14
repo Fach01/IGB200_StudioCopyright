6c0964bf1d077300884fc884e414bf718c83fea8
livlawsons <162529486+livlawsons@users.noreply.github.com>
2024-10-14 22:11:55 +1000

    flush update

--- Assets/Scripts/Abilities/Flush.cs
+++ Assets/Scripts/Abilities/Flush.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -66,7 +67,11 @@ public override void ActivateAbility(PlayerManager playerManager, GameObject Abi
         } 
     }
 
-    
+    private bool IsArrayFull(GameObject[] array)
+    {
+        return array.All(slot => slot != null);
+    }
+
 
     //TODO check whether the card being played is still activated and if thats the issue
     IEnumerator SelectCard(PlayerManager playerManager, int numDraw, int numDisc, GameObject AbilityUI, bool confirmed)
@@ -74,33 +79,61 @@ public override void ActivateAbility(PlayerManager playerManager, GameObject Abi
         GameObject[] discardCards = new GameObject[numDisc];
         while (!confirmed)
         {
-            if (playerManager.selectedCard == null || !playerManager.hand.GetComponent<HandManager>().SearchForCard(playerManager.selectedCard))
+            if (IsArrayFull(discardCards))
             {
-                yield return null;
-            }
-            else {
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
                     confirmed = true;
-                } 
+                }
+            }
+        
+            if (playerManager.selectedCard != null)
+            {
+                if (!IsArrayFull(discardCards))
+                {
+                    for (int i = 0; i < numDisc; i++)
+                    {
+                        if (discardCards[i] == null)
+                        {
+                            discardCards[i] = playerManager.selectedCard;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    // move all cards forward one slot
+                    for (int i = 1; i < numDisc; i++)
+                    {
+                        discardCards[i - 1] = discardCards[i];
+                    }
+
+                    // add new card in last slot
+                    discardCards[numDisc - 1] = playerManager.selectedCard;
+                }
             }
             yield return null;
         }
 
-        if (playerManager.selectedCard != null)
+        foreach (GameObject card in discardCards)
         {
-            playerManager.selectedCard.GetComponent<CardManager>().cardanimator.SetBool("Flush", true);
-
-            AudioManager.instance.PlaySFX("Flush");
-            for (int i = 0; i < numDraw; i++)
+            if (card != null)
             {
-                playerManager.DrawCard();
+                Destroy(card);
             }
-
-            playerManager.playField.GetComponent<Button>().interactable = true;
-            AbilityUI.GetComponent<AbilityUI>().Reset();
-
-            yield break;
         }
+
+        //playerManager.selectedCard.GetComponent<CardManager>().cardanimator.SetBool("Flush", true);
+
+        AudioManager.instance.PlaySFX("Flush");
+        for (int i = 0; i < numDraw; i++)
+        {
+            playerManager.DrawCard();
+        }
+
+        playerManager.playField.GetComponent<Button>().interactable = true;
+        AbilityUI.GetComponent<AbilityUI>().Reset();
+
+        yield break;
     }
 }
